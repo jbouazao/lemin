7036a41b25ba038b8a0ef1869a915964cb00aa66
Jihad Bouazaoui <jbouazao@e1r5p6.1337.ma>
2020-01-11 15:04:46 +0100

    edited f_init.c

--- f_init.c
+++ f_init.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   f_init.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: yjouaoud <yjouaoud@student.42.fr>          +#+  +:+       +#+        */
+/*   By: jbouazao <jbouazao@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/12/03 13:02:00 by yjouaoud          #+#    #+#             */
-/*   Updated: 2019/12/03 16:44:57 by yjouaoud         ###   ########.fr       */
+/*   Updated: 2020/01/11 15:04:10 by jbouazao         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,12 @@
 
 void	init_t_s(t_s *dt)
 {
-	dt->ants = 0;
+	dt->st = NULL;
+	dt->end = NULL;
 }
 
 void	init_t_rooms(t_rooms *rm)
 {
 	rm->name = NULL;
 	rm->next = NULL;
-	rm->st = NULL;
-	rm->end = NULL;
-	rm->st = NULL;
 }
